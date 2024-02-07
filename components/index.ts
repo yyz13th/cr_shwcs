9f9a0e5b438e06380b51807fbf10d656736010c3
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-07 15:34:02 +0300

    :duck:

---
+++ components/index.ts
@@ -0,0 +1,4 @@
+export {default as Hero} from '@/components/hero.component';
+export {default as CustomButton} from '@/components/custombutton.component'
+export {default as Navbar} from '@/components/navbar.component'
+export {default as Footer} from '@/components/footer.component'
