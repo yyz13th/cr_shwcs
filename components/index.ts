1adefde38f51b80cce540ccdbc53d0683f74e37f
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-02-08 16:23:51 +0300

    searchbar buildup

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,6 @@ export {default as Hero} from '@/components/hero.component';
 export {default as CustomButton} from '@/components/custombutton.component'
 export {default as Navbar} from '@/components/navbar.component'
 export {default as Footer} from '@/components/footer.component'
+export {default as SearchBar} from '@/components/searchbar.component'
+export {default as CustomFilter} from '@/components/customfilter.component'
+export {default as SearchManufacturer} from '@/components/searchmanufacturer.component'
